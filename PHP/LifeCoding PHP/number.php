b212df2ee8e9d43f0184aada681cf32981f4ab34
bomcoding <bomcoding@gmail.com>
2019-08-29 23:57:02 +0900

    Study PHP 1day_LifeCoding - echo - Number

---
+++ PHP/LifeCoding PHP/number.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body> <!-- php로 숫자연산해보기 -->
+    <h1>Number & Operator</h1>
+    <h2>1+1</h2>
+<?php
+    //출력방법 echo, pring()
+    echo 1+1;  
+?>    
+<h2>2-1</h2>
+<?php
+    echo 2-1;
+?>
+<h2>2*2</h2>
+<?php
+    echo 2*2;
+?>
+<h2>4/2</h2>
+<?php
+    echo 4/2;
+?>
+</body>
+</html>
